bea4e768707477c6fe20e232963d5da13d207921
duttarnab <arnab.bdutta@gmail.com>
2025-09-13 00:35:24 +0530

    remove binaries

--- server/dist/controllers/authController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare class AuthController {
-    static authorize(req: Request, res: Response): Promise<void | Response<any, Record<string, any>>>;
-    static token(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    static userInfo(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-}
-//# sourceMappingURL=authController.d.ts.map
--- server/dist/controllers/clientController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare class ClientController {
-    static createClient(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    static getClients(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    static deleteClient(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-}
-//# sourceMappingURL=clientController.d.ts.map
